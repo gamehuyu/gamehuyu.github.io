67a04e61cf334df4bd14a4e2b71d24cdb2a944a9
gamehuyu <gameapps222xp@163.com>
2025-03-13 02:58:52 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var BaseUrl =  "http://111.180.193.123:1673";
+var ResourceUrl = 'http://111.180.193.123:1888/ttxindaoyou/';
+
+if(window.location.pathname!='/'
+    &&window.location.pathname!='/index.html'
+    &&window.location.pathname!='/site/login.html'
+    &&window.location.pathname!='/site/wxredd.html')
+{
+    var wwid=window.localStorage.getItem("wwid");
+    if(!wwid)
+    {
+        localStorage.setItem('enter_url',encodeURIComponent(window.location.href))
+
+        alert('请先登录');
+        window.location.href = '/index.html';
+    }
+}
+
+
+        
